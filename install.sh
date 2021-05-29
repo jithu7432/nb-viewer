f3b78a7e1f9292b0deb2adbc246bd0a18732add5
Jithin Johnson <jithu7432@gmail.com>
2021-05-29 23:11:53 +0530

    install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mypath=$HOME
+depath='/opt/nbv/'
+echo "Installing dependencies.."
+pip3 install -r requirements.txt
+echo "Making Directory /opt/nbv/ .."
+mkdir $depath
+echo "Copying files to /opt/nbv/ .."
+sudo cp -r ./ico $depath
+sudo cp ./LICENSE $depath
+sudo cp ./nbv.desktop $mypath/.local/share/applications/
+sudo cp ./nbvs.py $depath
+sudo cp ./path.py $depath
+sudo cp ./README.md $depath
+echo "Installation Successfull"
+exit
+
