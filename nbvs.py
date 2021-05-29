7cdef8ea0e46f0f8cc21a073e49cdef6fa7f50b7
Jithin Johnson <jithu7432@gmail.com>
2021-05-29 20:46:17 +0530

    exec

--- nbvs.py
+++ nbvs.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
+
 import subprocess
 import webbrowser
 import sys
 import os
 import path
+import webview
+
+
 
 notebook = sys.argv[1]
-convert = path.path_to_jupyter + ' nbconvert --to html ' + notebook
-subprocess.call(convert.split())
-html = notebook[:-5] + 'html'
-file = webbrowser.open(html, 2)
-os.remove(html)
 
+htmlname = '.' + notebook.split('/')[-1][:-5] + 'html'
+htmlpath = "/".join(notebook.split('/')[:-1]) + '/'
+htmlfile = htmlpath + htmlname
+notebook_ = notebook.split('/')[-1]
+
+temp = open(htmlfile, 'w+')
+
+convert = path.path_to_jupyter + ' nbconvert --to html ' + notebook + ' --stdout'
+
+out = subprocess.call(convert.split(), stdout = temp)
+
+temp.close()
+
+#Pywebview
+
+
+window = webview.create_window(title = 'Jupyter Notebook Viewer - ' + notebook_, url = htmlfile, text_select = True, height = 1366, width = 632)
+webview.start()
+
+os.remove(htmlfile)
--- path.py
+++ path.py
@@ -1,2 +1,7 @@
-path_to_jupyter = '/home/jithin/venv/bin/jupyter'
+"""
+You may have installed jupyter in a virtual environment, therefore update the
+path in the variable and delete the second line.
+"""
+# path_to_jupyter = '/home/jithin/venv/bin/jupyter'
+path_to_jupyter = '/usr/bin/jupyter' # default 
 
