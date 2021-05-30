022488383a63672e77c822bff4fcd85634d7268a
Jithin Johnson <jithu7432@gmail.com>
2021-05-30 17:11:31 +0530

    final commit

--- path.py
+++ path.py
@@ -1,7 +1,16 @@
 """
 You may have installed jupyter in a virtual environment, therefore update the
 path in the variable and delete the second line.
-"""
-# path_to_jupyter = '/home/jithin/venv/bin/jupyter'
-path_to_jupyter = '/usr/bin/jupyter' # default 
 
+Path to jupyter can be obtained by the command 'which jupyter' on your terminal.
+
+"""
+import os 
+
+path_to_jupyter = os.getenv('HOME') + '/.local/bin/jupyter'
+
+# path_to_jupyter = '/home/jithin/venv/bin/jupyter'
+# path_to_jupyter = '/usr/bin/jupyter' # default 
+
+
+print(path_to_jupyter)
