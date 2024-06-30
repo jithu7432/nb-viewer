637554a9bbd89dbc3983c6c95ce376543d51996f
Jithin Johnson <76789703+jithu7432@users.noreply.github.com>
2024-06-30 12:37:24 +0530

    Added workflow for building (#10)

--- src/nbviewer/main.py
+++ src/nbviewer/main.py
@@ -11,17 +11,22 @@ from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog
 
 os.environ["QTWEBENGINE_DISABLE_SANDBOX"] = "1"
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+os.environ["QT_SCALE_FACTOR"] = "1"
 
-class Window(QWebEngineView):
+WIDTH, HEIGHT  = 0, 0
+
+class MainWindow(QWebEngineView):
     def __init__(self):
-        super(Window, self).__init__()
+        global HEIGHT
+        global WIDTH
+        super(MainWindow, self).__init__()
         self.setWindowTitle("JUPYTER NOTEBOOK VIEWER")
-        self.resize(800, 600)
-        self.setZoomFactor(1.15)
+        self.resize(HEIGHT // 2, WIDTH // 2)
         self.showMaximized()
 
 
-class App(QWidget):
+class FilePicker(QWidget):
     def __init__(self):
         super().__init__()
         self.title = "JUPYTER NOTEBOOK VIEWER"
@@ -30,7 +35,6 @@ class App(QWidget):
 
     def initialze(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(0, 0, 600, 100)
         self.open_filename_dialog()
 
     def open_filename_dialog(self):
@@ -57,7 +61,7 @@ def execute(app, file_path):
     with open(filename, "w") as file:
         file.write(body)
 
-    wind = Window()
+    wind = MainWindow()
     wind.setUrl(QUrl.fromLocalFile(filename))
     wind.show()
 
@@ -65,10 +69,16 @@ def execute(app, file_path):
 
 
 def main():
+    global WIDTH
+    global HEIGHT
+
     app = QApplication(sys.argv)
+    size = app.primaryScreen().size();
+
+    WIDTH, HEIGHT = size.width(), size.height()
 
     if len(sys.argv) == 1:
-        ex = App()
+        ex = FilePicker()
         if ex.filename:
             execute(app, ex.filename)
         else:
